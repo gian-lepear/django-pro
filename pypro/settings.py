99c08810d45d8ed1b51ca076482a5f6208a1b1b0
Giancarlo Lester <gian.lepear@gmail.com>
2020-10-11 13:26:17 -0300

    Adiçao do .env e pasta contrib

--- pypro/settings.py
+++ pypro/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "9_v)vdv5)z7+b@nwe@l=j_%_nu$j=jg^*038dyo$otyc7&z9@$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
